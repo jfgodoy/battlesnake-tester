5350eef4cf7676858e96026f4d76f17306185907
Jorge Godoy <godoy.jf@gmail.com>
2021-08-28 19:05:31 -0400

    fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
 
-
+declare var process : {
+  env: {
+    VITE_BASE: string | undefined
+  }
+}
 
 export default defineConfig({
+  base: process.env.VITE_BASE || "/",
   plugins: [solidPlugin(), solidSvg()],
   build: {
     target: "esnext",

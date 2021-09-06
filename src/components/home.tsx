dc4c8e14977bbd0fca1872766187ae3d1e57a4c3
Jorge Godoy <godoy.jf@gmail.com>
2021-09-06 18:59:55 -0300

    update home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,15 +6,19 @@ export default function Home(): JSX.Element {
       <h1 class="py-4 text-2xl text-gray-500 font-semibold">Welcome!</h1>
       <p>
         This app allow you to import games played in <a target="_blank" class="text-purple-600" href="https://play.battlesnake.com/">https://play.battlesnake.com/</a>, navigate all the turns and create tests
-    for your server. The test created are saved locally in your browser in a IndexedDB database.
+    for your server. The tests created are saved locally in your browser in a IndexedDB database.
       </p>
       <h2 class="py-4 text-xl">Get's start</h2>
       <ul>
         <li>1.- config your server on the top left input.</li>
-        <li>2.- import a game from <a target="_blank" class="text-purple-600" href="https://play.battlesnake.com/">https://play.battlesnake.com/</a>.</li>
+        <li>2.- create a new test importing a game from <a target="_blank" class="text-purple-600" href="https://play.battlesnake.com/">https://play.battlesnake.com/</a>.</li>
         <li>3.- Set the name and expected result.</li>
       </ul>
-      <p class="my-2">That's all! add as many test as you want</p>
+      <p class="my-2">That's all! You can run a single test or all at once.</p>
+
+      <p class="mt-4">
+        This project is in active development. Visit the <a target="_blank" class="text-purple-600" href="https://github.com/jfgodoy/battlesnake-tester">github repository</a> and leave suggestions of new features.
+      </p>
     </div>
     );
 }
